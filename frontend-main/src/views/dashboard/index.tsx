9e0b82acb36299c10181910af1fc47995ffc00ee
SerBABA <32250073+SerBABA@users.noreply.github.com>
2021-09-16 20:36:08 +1200

    Began work on navbar toggling

--- frontend-main/src/views/dashboard/index.tsx
+++ frontend-main/src/views/dashboard/index.tsx
@@ -1,36 +1,10 @@
-import { useLogoutMutation, useUsersQuery } from "generated/graphql";
 import React from "react";
-import { RouteComponentProps } from "react-router";
+import Navbar from "components/Navbar";
 
-export const Dashboard: React.FC<RouteComponentProps> = ({ history }) => {
-  const { data, loading } = useUsersQuery();
-  const [logout] = useLogoutMutation({ fetchPolicy: "network-only" });
-
-  if (loading || !data) {
-    return <div>loading...</div>;
-  }
-
-  const handleLogout = async () => {
-    await logout()
-      .then((data) => {
-        console.log(data);
-        history.push("/");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
+export const Dashboard: React.FC = () => {
   return (
     <div>
-      <ul>
-        {data.users.map((user) => (
-          <li key={user.id}>
-            {user.id},{user.username}
-          </li>
-        ))}
-      </ul>
-      <button onClick={handleLogout}>LOGOUT</button>
+      <Navbar></Navbar>
     </div>
   );
 };
